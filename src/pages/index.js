c4933cd7b7f8b9872ae19caaf2970a58ea57a568
kacperf531 <k.frackowski@livechatinc.com>
2021-01-19 13:56:10 +0100

    minor changes in content

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ const Index = ({ data }) => (
       <Title as="h2" size="large">
         {data.homeJson.content.childMarkdownRemark.rawMarkdownBody}
       </Title>
-      <Title style="small"><br/>Co mają ze sobą wspólnego prawa kobiet i prawa zwierząt? Wszystko. <br/>O podobieństwach między opresją kobiet i zwierząt. Dyskryminacja kobiet jest strukturalna, podobnie jak dyskryminacja zwierząt. Te drugie są jednak ofiarami meta wykluczenia: odmawia się im nawet statusu ofiary, przemoc wobec nich jest znormalizowana. Możemy oczekiwać od feministek, podobnie jak od innych osób walczących o sprawiedliwość społeczną, że znając i rozumiejąc mechanizmy wykluczenia, dostrzegą je wszędzie tam gdzie one są i odmowy  uczestniczenia w dyskryminacji innych, co jest naturalnym zachowaniem emancypacyjnym.</Title>
+      <Title style="small"><br/>Co mają ze sobą wspólnego prawa kobiet i prawa zwierząt? Wszystko. <br/> Dyskryminacja kobiet jest strukturalna, podobnie jak dyskryminacja zwierząt. Te drugie są jednak ofiarami meta wykluczenia: odmawia się im nawet statusu ofiary, przemoc wobec nich jest znormalizowana. Możemy oczekiwać od feministek, podobnie jak od innych osób walczących o sprawiedliwość społeczną, że znając i rozumiejąc mechanizmy wykluczenia, dostrzegą je wszędzie tam gdzie one są i odmowy  uczestniczenia w dyskryminacji innych, co jest naturalnym zachowaniem emancypacyjnym.</Title>
       <br/> - J.Wiśniewska<br/> 
       <Modal>
         <Title as="h1" size="small">Sobota, 23.01.2021</Title>
@@ -22,6 +22,7 @@ const Index = ({ data }) => (
         <Title as="h1" size="small">Niedziela, 24.01.2021</Title>
         <p><br/>Jaki jest problem z byciem suką? W poszukiwaniu językowych manipulacji. (J. Wiśniewska), godzina 18:00<br/></p>
         <p><br/>Females support females - dumne hasło utopione w szklance mleka. (M. Kohut), godzina 20:00<br/></p>
+        <Title as="h1" size="small">Linki do spotkań podamy na 30 minut przed rozpoczęciem na stronie i na fb/twitterze.</Title>
       </Modal>
     </Box>
     <Gallery items={data.homeJson.gallery} />
